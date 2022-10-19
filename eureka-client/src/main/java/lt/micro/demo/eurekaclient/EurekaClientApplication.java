c3c2c8159d7ed9c74537525bf30bd0b2b8f5460b
marinaM2019 <morkeliuniene.marina@gmail.com>
2022-10-19 23:01:19 +0300

    add files

---
+++ eureka-client/src/main/java/lt/micro/demo/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,16 @@
+package lt.micro.demo.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
